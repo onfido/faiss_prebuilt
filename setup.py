f37d3a8ad2bf57db4d9d3b86bedc7b01a2f77589
Tom Forbes <tom.forbes@onfido.com>
2019-04-16 15:02:53 +0100

    Publish to `faiss` instead of `faiss-prebuilt`

--- setup.py
+++ setup.py
@@ -18,12 +18,13 @@ class BinaryDistribution(Distribution):
         return True
 
 
-long_description="""
-*Unofficial prebuilt binary*
+long_description = """
+## Unofficial prebuilt binary for Linux and MacOS
 
-The repo that builds this project can be found here: https://github.com/onfido/faiss_prebuilt
+The repo that builds this project can be found here: 
+[https://github.com/onfido/faiss_prebuilt](https://github.com/onfido/faiss_prebuilt)
 
-Original readme:
+## Original readme:
 
 Faiss is a library for efficient similarity search and clustering of dense 
 vectors. It contains algorithms that search in sets of vectors of any size,
@@ -32,11 +33,13 @@ code for evaluation and parameter tuning. Faiss is written in C++ with
 complete wrappers for Python/numpy. Some of the most useful algorithms 
 are implemented on the GPU. It is developed by Facebook AI Research.
 """
+
 setup(
-    name='faiss_prebuilt',
+    name='faiss',
     version='1.5.2',
     description='A library for efficient similarity search and clustering of dense vectors',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/facebookresearch/faiss',
     author='Matthijs Douze, Jeff Johnson, Herve Jegou',
     author_email='matthijs@fb.com',
@@ -48,6 +51,7 @@ setup(
         'faiss': ['*.so'],
     },
     classifiers=[
+     'Programming Language :: Python :: 2.7',
      'Programming Language :: Python :: 3.5',
      'Programming Language :: Python :: 3.6',
      'Programming Language :: Python :: 3.7'
